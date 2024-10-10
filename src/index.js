62649d53db1969e82e5cf1cd4caf6ed746d6a36b
Claudia Pando <claudiamfp@hotmail.com>
2024-10-10 23:32:14 +0100

    solved lab

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
+const startButton = document.querySelector('button');
+startButton.addEventListener('click', () => {
+startCountdown();  
+});
+
 
 
 
@@ -17,16 +22,46 @@ function startCountdown() {
 
 
   // Your code goes here ...
+
+  const timeDisplay = document.getElementById ('time');
+  startButton.disabled = true;
+
+
+  timer = setInterval(function () {
+  console.log(remainingTime);
+  remainingTime--;
+  timeDisplay.innerHTML = remainingTime;
+
+
+  if (remainingTime <= 0) {
+    clearInterval(timer);
+    showToast('Lift off! 🚀');
+    startButton.disabled = false;
+    remainingTime = DURATION;
+  }
+  }, 1000);
 }
-
-
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
+  console.log("showToast called!")
+
 
   // Your code goes here ...
+
+  const toastElement = document.getElementById('toast');
+  const toastMessage = document.getElementById('toast-message');
+
+  toastMessage.innerHTML = message;
+  toastElement.classList.add('show');
+
+
+  setTimeout(() => {
+    toastElement.classList.remove('show');
+  }, 3000);
+}
+
 
 
 
@@ -35,4 +70,4 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
